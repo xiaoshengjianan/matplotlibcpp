6e96df1ed762b1507828d2e7effe1d6b6dc389e7
xiaoshengjianan <1719982220@qq.com>
2023-10-07 10:05:11 +0800

    first commit

---
+++ build/example/CMakeFiles/modern.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jane/matplotlib-cpp-gai/example/src/modern.cpp" "/home/jane/matplotlib-cpp-gai/build/example/CMakeFiles/modern.dir/src/modern.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../example/include"
+  "../matplotlib/include"
+  "/usr/include/python3.6m"
+  "/home/jane/.local/lib/python3.6/site-packages/numpy/core/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/example/CMakeFiles/modern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modern.dir/src/modern.cpp.o"
+  "modern"
+  "modern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
